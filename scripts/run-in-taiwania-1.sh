48f43526f3fd0af841033dd1fe8d2a91a5116dcc
cliffxzx <cliffxzx@gmail.com>
2022-10-22 07:23:25 +0000

    Fix race condition

--- scripts/run-in-taiwania-1.sh
+++ scripts/run-in-taiwania-1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 REPO_FOLDER=$(dirname $(dirname $(readlink -f $0)))
-JOB_NAME=QNet
+JOB_NAME=$(sed 's/ /,/g' <<< "$@")
 NOTIFICATION_EMAIL=
 
 CONDA_ENV_NAME=QNetPy8
@@ -10,10 +10,14 @@ CONDA_ENV_PATH=~/QNetPy8
 NODE_NAME=ct160
 NODE_COUNT=4
 NCUPS_PER_NODE=40
+RUN_ID=$(date +%s%N)
 
-LOG_FILE=~/run.$(date +%s).log
+LOG_FILE=~/.pbs_history/log/run.$(date +%s%N).log
 
 PROJECT_ID=MST111245
+
+mkdir -p ~/.pbs_history/script
+mkdir -p ~/.pbs_history/log
 
 ARGS=()
 while [[ $# -gt 0 ]]; do
@@ -53,7 +57,7 @@ export OMP_NUM_THREADS=$NCUPS_PER_NODE
 export KMP_BLOCKTIME=30
 #run python script with inputs from this .sh script
 $CONDA_ENV_PATH/bin/python $REPO_FOLDER/train.py \$@ $@
-""" > ~/run.sh
+""" > ~/.pbs_history/script/run.$RUN_ID.sh
 
 EMAIL_REGEX="^[a-zA-Z0-9\._-]+\@[a-zA-Z0-9._-]+\.[a-zA-Z]+\$"
 echo '#!/bin/bash'"""
@@ -79,14 +83,14 @@ do
   then
     # ssh into each node and run the .sh script with node info
     # run in background
-    ssh \$node \"~/run.sh --distributed_node_index \$C --distributed_nodes \${NODES[*]} $@\" &
+    ssh \$node \"~/.pbs_history/script/run.$RUN_ID.sh --distributed_node_index \$C --distributed_nodes \${NODES[*]} $@\" &
     C=\$((\$C+1))
     sleep 2
   fi
 done
-~/run.sh --distributed_node_index 0 --distributed_nodes \${NODES[*]} $@ >> $LOG_FILE 2>&1
+~/.pbs_history/script/run.$RUN_ID.sh --distributed_node_index 0 --distributed_nodes \${NODES[*]} $@ >> $LOG_FILE 2>&1
 echo \"Done with PBS\"
-""" > ~/pbs.sh
+""" > ~/.pbs_history/script/pbs.$RUN_ID.sh
 
 module load anaconda3/5.1.10
 
@@ -95,11 +99,9 @@ if ! { conda env list | grep $CONDA_ENV_NAME; } >/dev/null 2>&1; then
   conda install -yp $CONDA_ENV_PATH python==3.8
 fi
 
-echo "Installing packages ..."
-$CONDA_ENV_PATH/bin/pip install -r $REPO_FOLDER/requirements.txt >/dev/null 2>&1
+echo "IMPORTANT! you should install pacakge before running:"
+echo "$CONDA_ENV_PATH/bin/pip install -r $REPO_FOLDER/requirements.txt >/dev/null 2>&1"
 
-chmod u+x ~/run.sh ~/pbs.sh
-echo "[Job ID: $(qsub ~/pbs.sh)] Start running ..." > $LOG_FILE 2>&1
-echo "Log file: $LOG_FILE, Plz use below command to view:"
+chmod u+x ~/.pbs_history/script/run.$RUN_ID.sh ~/.pbs_history/script/pbs.$RUN_ID.sh
+echo "[Job ID: $(qsub ~/.pbs_history/script/pbs.$RUN_ID.sh)] Start running..." > $LOG_FILE 2>&1
 echo "tail -f $LOG_FILE"
-# tail -f $LOG_FILE
