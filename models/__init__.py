d99cc031c971c8bf462e4e03745b4ee6040bfa24
David Day <dj6082013@gmail.com>
2022-11-05 18:22:25 +0800

    Count number of parameters w/o embeddings.

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from .FNet import FNet
 from .QNet import QNetEncoder
 from .QLSTM import QLSTMEncoder
@@ -13,9 +14,27 @@ mapping = {
 def get_model(args, vocab_size: int):
     model = mapping[args.model]
     if args.model == 'qnet':
-        return model(vocab_size, args.seq_len, args.embed_size, args.num_blocks, args.qnet_depth)
+        instance = model(vocab_size, args.seq_len, args.embed_size, args.num_blocks, args.qnet_depth)
     else:
-        return model(vocab_size, args.seq_len, args.embed_size, args.num_blocks)
+        instance = model(vocab_size, args.seq_len, args.embed_size, args.num_blocks)
+
+    # Build model with one inference
+    instance(tf.ones((1, args.seq_len), dtype=tf.int64))
+
+    # Count parameters
+    total_parameters = 0
+    for variable in instance.variables:
+        if 'embedding' in variable.name:
+            continue
+        # shape is an array of tf.Dimension
+        shape = variable.get_shape()
+        variable_parameters = 1
+        for dim in shape:
+            variable_parameters *= dim
+        total_parameters += variable_parameters
+    print('Number of Parameters w/o embedding layer:', total_parameters)
+
+    return instance
 
 def list_model():
     return mapping.keys()
