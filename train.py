0fa17da92bdff9032108ba164b10f7cce6d36bf1
David Day <dj6082013@gmail.com>
2022-11-10 16:54:42 +0800

    Add record.csv

--- train.py
+++ train.py
@@ -3,22 +3,25 @@
 from utils.args_parser import solve_args
 args = solve_args(multi_worker_strategy=True)
 
-import os, time, json
+import os, time, json, csv
+from pathlib import Path
 
 import numpy as np
 import tensorflow as tf
 from datasets import get_dataset
 from trainers import get_trainer
 
-def save_log(history, val_metric: str=None):
+def save_log(history, val_metric: str='val_loss'):
     logs = {
         'config': vars(args),
         'history': history,
     }
 
-    if val_metric != None:
+    if val_metric == 'val_loss':
+        logs['best_acc'] = min(history[val_metric])
+    else:
         logs['best_acc'] = max(history[val_metric])
-        print('Best score: ', logs['best_acc'])
+    print('Best score: ', logs['best_acc'])
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     logfile_name = dir_path + f'/logs/{args.model}-{int(time.time())}.json'
@@ -26,6 +29,28 @@ def save_log(history, val_metric: str=None):
     with open(logfile_name, 'w') as f:
         json.dump(logs, f, indent=4)
     print('Log file saved at: ', logfile_name)
+
+    record = {
+        'Complete Time': time.ctime(),
+        'Model':args.model,
+        'Dataset':args.dataset,
+        'Embed size':args.embed_size,
+        'Num block': args.num_blocks,
+        'Best Score': logs['best_acc'],
+        'args': json.dumps(vars(args))
+    }
+
+    record_csv = dir_path + f'/logs/0-Record.csv'
+    Path(record_csv).touch()
+    with open(record_csv, 'r') as f:
+        rows = list(csv.DictReader(f))
+    with open(record_csv, 'w') as f:
+        writer = csv.DictWriter(f, record.keys())
+        writer.writeheader()
+        for row in rows:
+            writer.writerow(row)
+        writer.writerow(record)
+    print('0-Record.csv updated')
 
 def main(args):
     # Set random seeds
