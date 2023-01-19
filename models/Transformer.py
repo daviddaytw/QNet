b6737296858901969a9e20447fca53ac540d5d09
David Day <dj6082013@gmail.com>
2023-01-19 12:39:00 +0800

    Set num_head of Transformer to 8.

--- models/Transformer.py
+++ models/Transformer.py
@@ -33,7 +33,7 @@ class TokenAndPositionEmbedding(layers.Layer):
         return x + positions
 
 class TransformerEncoder(layers.Layer):
-    def __init__(self, vocab_size: int, maxlen: int, embed_dim: int, num_blocks: int, num_heads: int = 1):
+    def __init__(self, vocab_size: int, maxlen: int, embed_dim: int, num_blocks: int, num_heads: int = 8):
         super(TransformerEncoder, self).__init__()
         self.layers = tf.keras.models.Sequential([
             layers.Input(shape=(maxlen,)),
